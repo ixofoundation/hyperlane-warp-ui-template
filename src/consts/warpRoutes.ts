35d87cc1169d13a7e473aee7f685b24c52193e11
Michael Pretorius <michaelpretorius96@gmail.com>
2025-02-18 16:35:44 +0200

    fix: add mainnet warp routes and chains

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -7,29 +7,57 @@ export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     //#region IXO
     // IXO pandora8 to basesepolia
+    // {
+    //   chainName: 'pandora',
+    //   standard: TokenStandard.CwHypCollateral,
+    //   decimals: 6,
+    //   symbol: 'IXO',
+    //   name: 'Ixo',
+    //   logoURI: '/logos/ixo_icon.svg',
+    //   addressOrDenom: 'ixo18hkdw62hjuh3pfz0d89hlq9ldlh6ekfvpf9gnun90sfcy0scgm9s66pptc',
+    //   collateralAddressOrDenom: 'uixo',
+    //   connections: [{ token: 'ethereum|basesepolia|0xa2718De82656C11eb467679A5A06e345a08E2836' }],
+    // },
+    // // IXO on basepolia from pandora8
+    // {
+    //   chainName: 'basesepolia',
+    //   standard: TokenStandard.EvmHypSynthetic,
+    //   decimals: 6,
+    //   symbol: 'IXO',
+    //   name: 'Hyperlane Bridged IXO Pandora',
+    //   logoURI: '/logos/ixo_icon.svg',
+    //   addressOrDenom: '0xa2718De82656C11eb467679A5A06e345a08E2836',
+    //   connections: [
+    //     {
+    //       token: 'cosmos|pandora|ixo18hkdw62hjuh3pfz0d89hlq9ldlh6ekfvpf9gnun90sfcy0scgm9s66pptc',
+    //     },
+    //   ],
+    // },
+
+    // IXO ixo to base
     {
-      chainName: 'pandora',
-      standard: TokenStandard.CwHypCollateral, // CosmosNative?
+      chainName: 'ixo',
+      standard: TokenStandard.CwHypCollateral,
       decimals: 6,
       symbol: 'IXO',
       name: 'Ixo',
       logoURI: '/logos/ixo_icon.svg',
-      addressOrDenom: 'ixo18hkdw62hjuh3pfz0d89hlq9ldlh6ekfvpf9gnun90sfcy0scgm9s66pptc',
+      addressOrDenom: 'ixo1ktamcsma2yyde49wuh7sv6jhw873f8melddgajnns48w7g8hs3pqdcs4uz',
       collateralAddressOrDenom: 'uixo',
-      connections: [{ token: 'ethereum|basesepolia|0xa2718De82656C11eb467679A5A06e345a08E2836' }],
+      connections: [{ token: 'ethereum|base|0x00536Dc7FFa96Eec458b353d0719949D4ACD23f3' }],
     },
-    // IXO on basepolia from pandora8
+    // IXO on base from ixo
     {
-      chainName: 'basesepolia',
+      chainName: 'base',
       standard: TokenStandard.EvmHypSynthetic,
       decimals: 6,
       symbol: 'IXO',
-      name: 'Hyperlane Bridged IXO Pandora',
+      name: 'Hyperlane Bridged IXO',
       logoURI: '/logos/ixo_icon.svg',
-      addressOrDenom: '0xa2718De82656C11eb467679A5A06e345a08E2836',
+      addressOrDenom: '0x00536Dc7FFa96Eec458b353d0719949D4ACD23f3',
       connections: [
         {
-          token: 'cosmos|pandora|ixo18hkdw62hjuh3pfz0d89hlq9ldlh6ekfvpf9gnun90sfcy0scgm9s66pptc',
+          token: 'cosmos|ixo|ixo1ktamcsma2yyde49wuh7sv6jhw873f8melddgajnns48w7g8hs3pqdcs4uz',
         },
       ],
     },
@@ -42,11 +70,23 @@ export const warpRouteConfigs: WarpCoreConfig = {
         amount: 27000,
         addressOrDenom: 'uixo',
       },
+      {
+        origin: 'ixo',
+        destination: 'base',
+        amount: 27000,
+        addressOrDenom: 'uixo',
+      },
     ],
     localFeeConstants: [
       {
         origin: 'pandora',
         destination: 'basesepolia',
+        amount: 27000,
+        addressOrDenom: 'uixo',
+      },
+      {
+        origin: 'ixo',
+        destination: 'base',
         amount: 27000,
         addressOrDenom: 'uixo',
       },
